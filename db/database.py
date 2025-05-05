6651ad8fef17358734a6ca7465cb822ee3e1a741
essayeswajih <wajihsayes@gmail.com>
2025-05-05 15:52:12 +0100

    rq.txt

--- db/database.py
+++ db/database.py
@@ -5,8 +5,10 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 load_dotenv()
 
-URL_DATABASE = os.getenv("URL_DATABASE") 
-engine = create_engine(URL_DATABASE)
+DATABASE_URL = os.environ.get("DATABASE_URL")
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL environment variable not set")
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
